21b386efe0f17b2651e3c5946b2e2d0d9cff85b5
Ali Nobariasl <freeeeboy.gh@gmail.com>
2023-06-15 17:18:02 +0300

    fixed media configurations

--- BookShop_main/BookShop_main/settings.py
+++ BookShop_main/BookShop_main/settings.py
@@ -122,3 +122,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# media configurations
+
+MEDIA_ROOT = BASE_DIR/'media' 
+MEDIA_URL = 'media/'
--- BookShop_main/BookShop_main/urls.py
+++ BookShop_main/BookShop_main/urls.py
@@ -1,6 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from .views import index
 
@@ -9,3 +12,8 @@ urlpatterns = [
     path('', index, name='index'),
     
 ]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
