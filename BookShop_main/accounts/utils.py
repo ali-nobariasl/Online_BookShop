844f613ef8f6e92380bf44afcaa29ea94a543c2c
Ali Nobariasl <freeeeboy.gh@gmail.com>
2023-06-27 14:32:11 +0300

    activating link funcion

--- BookShop_main/accounts/utils.py
+++ BookShop_main/accounts/utils.py
@@ -1,6 +1,6 @@
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_decode
+from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
@@ -22,15 +22,16 @@ def detectUrl(user):
 
 def send_verification_email(request, user):
     
+    from_email = 'Bookstore'
     current_site = get_current_site(request)
     mail_subject = 'Please activate your account'
     message= render_to_string(
         'account/emails/account_verification_email.html',
         {'user': user,
         'domain': current_site,
-        'uid':urlsafe_base64_decode(force_bytes(user.pk)),
+        'uid':urlsafe_base64_encode(force_bytes(user.pk)),
         'token': default_token_generator.make_token(user),
         })
     to_mail = user.email
-    mail = EmailMessage(mail_subject, message,to=[to_mail])
+    mail = EmailMessage(mail_subject, message,from_email, to=[to_mail])
     mail.send()
--- BookShop_main/accounts/views.py
+++ BookShop_main/accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from  django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required , user_passes_test
 from django.core.exceptions import PermissionDenied
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
 
 from .forms import UserForm
 from .models import User , UserProfile
@@ -168,5 +170,18 @@ def vendorDashboard(request):
 
 def activate(request,uid64,token):
     # activate the user 
+    try:
+        uid = urlsafe_base64_decode(uid64).decode()
+        user = User._default_manager.get(pk = uid)
+    except(ValueError, TypeError, OverflowError, User.DoesNotExist):
+        user = None
     
-    return 
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request,'Your account activated successfully')
+        return redirect('myAccount')
+    else:
+        messages.error(request,'the verification link is not correct')
+        return redirect('myAccount')
+    
