d4c44e9ce6357afd2fee87f037601ac61ffef7ab
Ali Nobariasl <freeeeboy.gh@gmail.com>
2023-06-30 16:47:02 +0300

    making dynamic url for images

--- BookShop_main/accounts/views.py
+++ BookShop_main/accounts/views.py
@@ -168,7 +168,11 @@ def custDashboard(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
-    return render(request, 'accounts/vendorDashboard.html')
+    vendor = Vendor.objects.get(user=request.user)
+    
+    
+    context = {'vendor':vendor}
+    return render(request, 'accounts/vendorDashboard.html', context=context)
 
 
 
