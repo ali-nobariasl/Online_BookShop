ecb27f72305bd984ab0befe7825d2ffb5d3efc3b
Ali Nobariasl <freeeeboy.gh@gmail.com>
2023-07-04 19:55:10 +0300

    vprofile function

--- BookShop_main/accounts/forms.py
+++ BookShop_main/accounts/forms.py
@@ -21,9 +21,12 @@ class UserForm(forms.ModelForm):
         if password != confirm_password:
             raise forms.ValidationError("passi jun is not match :)")
         
+ 
+ 
         
 class UserProfileForm(forms.ModelForm):
-    
+    profile_picture = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
+    cover_photo = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
     class Meta:
         model= UserProfile
         fields = ['profile_picture','cover_photo','address_line_1','address_line_2', 'country','state','city','pin_code','latitude','longitude']
--- BookShop_main/vendor/views.py
+++ BookShop_main/vendor/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render , get_object_or_404
+from django.shortcuts import render , get_object_or_404 , redirect
 from django.contrib import messages
 
 from .forms import VendorForm
@@ -10,28 +10,26 @@ from accounts.models import UserProfile
 
 def vprofile(request):
     
-    
     profile = get_object_or_404(UserProfile, user=request.user)
     vendor = get_object_or_404(Vendor, user = request.user)
     
-    profile_form = UserProfileForm(instance= profile)
-    vendor_form = VendorForm(instance=vendor)
-    
-    
     if request.method == 'POST':
-        profile_form = UserProfileForm(request.POST,)
-        vendor_form = VendorForm(request.POST)
+        profile_form = UserProfileForm(request.POST,request.FILES,instance= profile)
+        vendor_form = VendorForm(request.POST,request.FILES,instance= vendor)
+        
         if vendor_form.is_valid() and profile_form.is_valid():
             vendor_form.save()
             profile_form.save()
-            
+            messages.success(request,'Your store has been updated successfully')   
+            return redirect('vprofile')   
         else:
             messages.error(request,'Invalid profile')
-        
-        
+            print(profile_form.errors)
+            print(vendor_form.errors)
+    else:
+        profile_form = UserProfileForm(instance= profile)
+        vendor_form = VendorForm(instance=vendor)  
             
-    
-    
     
     context = {
         'profile_form': profile_form,
