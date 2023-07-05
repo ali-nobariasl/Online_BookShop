efe1be0fe00c24aab48c2986cb05cc824913cc21
Ali Nobariasl <freeeeboy.gh@gmail.com>
2023-07-05 23:12:50 +0300

    first way to make read only

--- BookShop_main/accounts/forms.py
+++ BookShop_main/accounts/forms.py
@@ -27,6 +27,8 @@ class UserForm(forms.ModelForm):
 class UserProfileForm(forms.ModelForm):
     profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class':'btn btn-info'}),validators=[allow_only_images_validator])
     cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class':'btn btn-info'}), validators=[allow_only_images_validator])
+    latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
     class Meta:
         model= UserProfile
         fields = ['profile_picture','cover_photo','address_line_1','address_line_2', 'country','state','city','pin_code','latitude','longitude']
