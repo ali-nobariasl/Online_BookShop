e2954ddef9591c7da8b64e55434bddc4eb70e51a
Ali Nobariasl <freeeeboy.gh@gmail.com>
2023-07-10 17:34:40 +0300

    accounts

--- BookShop_main/marketplace/views.py
+++ BookShop_main/marketplace/views.py
@@ -6,7 +6,9 @@ from vendor.models import Vendor
 
 def marketplace(request):
     
-    
-    marketplace = Vendor.objects.filter()
-    context = {'marketplace':marketplace}
+    vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)
+    vendor_count = vendors.count()
+    context = {'vendors':vendors,
+               'vendor_count':vendor_count,
+               }
     return render(request, 'marketplace/listing.html', context=context)
