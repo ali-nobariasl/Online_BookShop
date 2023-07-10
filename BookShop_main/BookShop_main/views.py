bb8cd8f291ea2de0087410a00f12e4ae5e3df39e
Ali Nobariasl <freeeeboy.gh@gmail.com>
2023-07-10 14:59:36 +0300

    s

--- BookShop_main/BookShop_main/views.py
+++ BookShop_main/BookShop_main/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from vendor.models import Vendor
+
 
 def index(request):
     
-    
-    return render(request,'home.html')
+    vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[0:8]
+    print(vendors)
+    context = {
+        'vendors': vendors,
+    }
+    return render(request,'home.html', context=context)
