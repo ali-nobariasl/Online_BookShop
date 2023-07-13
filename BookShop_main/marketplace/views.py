c947c34dde769274ee7adcb8df0005ec44922420
Ali Nobariasl <freeeeboy.gh@gmail.com>
2023-07-13 13:44:07 +0300

    add to cart function

--- BookShop_main/marketplace/views.py
+++ BookShop_main/marketplace/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render,get_object_or_404,HttpResponse
+from django.shortcuts import render,get_object_or_404
 from django.db.models import Prefetch
+from django.http import JsonResponse, HttpResponse
 
 from vendor.models import Vendor
 from stok.models import Category, BookItem
-
+from .models import Cart
 
 
 def marketplace  (request):
@@ -36,4 +37,24 @@ def vendor_detail(request,vendor_slug):
 
 def add_to_cart(request,book_id):
     
-    return HttpResponse("testing")
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            
+            try:
+                bookitem = BookItem.objects.get(id=book_id)
+                # check if book has already been added to the cart before
+                try:
+                    chkcat = Cart.objects.get(user= request.user, bookitem=bookitem)
+                    chkcat.quantity += 1
+                    chkcat.save()
+                    return JsonResponse({'status':'Success','message':'quantity increased'})
+                except:
+                    chkcat = Cart.objects.create(user= request.user, bookitem=bookitem, quantity=1)
+                    return JsonResponse({'status':'Success','message':'cart created and quantity increased'})
+                    
+            except:
+                return JsonResponse({'status':'failed','message':'this item is not exist'})
+        else:
+            return JsonResponse({'status':'failed','message':'Invalid request.'})     
+    else:
+        return JsonResponse({'status':'failed','message':'Please log in, bfore any actions.'})
