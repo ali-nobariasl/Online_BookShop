fd1483514f333e9c90e1b3db1b9e090eda787c9c
Ali Nobariasl <freeeeboy.gh@gmail.com>
2023-07-27 13:49:24 +0300

    d

--- BookShop_main/BookShop_main/views.py
+++ BookShop_main/BookShop_main/views.py
@@ -4,22 +4,35 @@ from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.measure import D  # ``D`` is a shortcut for ``Distance``
 from django.contrib.gis.db.models.functions import Distance
 
-
 from vendor.models import Vendor
 
 
-def index(request):
+
+def get_or_set_current_location(request):
+    if 'lat' in request.session:
+        lat = request.session['lat']
+        lng = request.session['lng']
+        return lng,lat
+    elif 'lat' in request.get:
+        lat = request.GET.get('lat')
+        lng = request.GET.get('lng')
+        request.session['lat'] = lat
+        request.session['lng'] = lng
+        return lng,lat
+    else:
+        return None
     
-    if 'lat' in request.GET:
-        lat = request.GET['lat']
-        lng = request.GET['lng']
+    
+def index(request):
+    if get_or_set_current_location(request) is not None:
         
-        pnt = GEOSGeometry('POINT(%s  %s)' % (lng, lat ))    
+        pnt = GEOSGeometry('POINT(%s  %s)' % (get_or_set_current_location(request)))  
+          
         vendors = Vendor.objects.filter(user_profile__location__distance_lte=(pnt,D(km=1000))).annotate(
             distance=Distance("user_profile__location",pnt)).order_by("distance")
             
         for v in vendors:
-                v.kms =v.distance.km
+            v.kms = round(v.distance.km,1)
     else:   
         vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[0:8]
     context = {
