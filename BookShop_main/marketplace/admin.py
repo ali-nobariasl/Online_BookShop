292aac8b543edf5083b97c3b925923c730f3b8d5
Ali Nobariasl <freeeeboy.gh@gmail.com>
2023-08-02 18:40:19 +0300

    tax model

--- BookShop_main/marketplace/admin.py
+++ BookShop_main/marketplace/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 
+from .models import Cart, Tax
 
-from .models import Cart
 
-
-class CartModel(admin.ModelAdmin):
+class CartAdmin(admin.ModelAdmin):
     list_display = ('user','bookitem','quantity','updated_at')
 
+class TaxAdmin(admin.ModelAdmin):
+    list_display =('tax_type','tax_percentage','is_active') 
 
-admin.site.register(Cart,CartModel)
+
+admin.site.register(Cart,CartAdmin)
+admin.site.register(Tax,TaxAdmin)
+
