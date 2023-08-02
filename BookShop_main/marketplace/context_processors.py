a41504ce56ebbfef20dc857c4b2ffffe0d1b1169
Ali Nobariasl <freeeeboy.gh@gmail.com>
2023-08-02 18:56:31 +0300

    get tax function

--- BookShop_main/marketplace/context_processors.py
+++ BookShop_main/marketplace/context_processors.py
@@ -1,5 +1,5 @@
 
-from .models import Cart
+from .models import Cart, Tax
 from stok.models import BookItem
 
 
@@ -26,16 +26,23 @@ def get_cart_amounts(request):
     subtotal =0
     tax =0
     grand_total =0
+    tax_dic = {}
     if request.user.is_authenticated:
         cart_items = Cart.objects.filter(user=request.user)
         for item in cart_items:
             subtotal += (item.bookitem.price * item.quantity )
         
+        get_tax = Tax.objects.filter(is_active=True)
+        for i in get_tax:
+            tax_type = i.tax_type
+            tax_percentage = i.tax_percentage
+            tax_amount = round((tax_percentage*subtotal)/100,2)
+            tax_dic.update({tax_type:{tax_percentage:tax_amount}})
+
+        for key in tax_dic.values():
+            for x in key.values():
+                tax = tax + x
+            
+
         grand_total = subtotal + tax
-        
-    
-    print(subtotal)
-    print(grand_total)
-    return dict(subtotal=subtotal,
-                grand_total=grand_total,
-                tax=tax)
+    return dict(subtotal=subtotal,grand_total=grand_total,tax=tax,tax_dic=tax_dic,)
