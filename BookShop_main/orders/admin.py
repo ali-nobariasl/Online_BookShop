2fa187273c9e9234d731aa01e75e353d3e8b4c48
Ali Nobariasl <freeeeboy.gh@gmail.com>
2023-08-03 16:59:01 +0300

    oerder models

--- BookShop_main/orders/admin.py
+++ BookShop_main/orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Payment, Order, OrderedBook
+
+
+
+admin.site.register(Payment)
+admin.site.register(Order)
+admin.site.register(OrderedBook)
