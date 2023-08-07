594cfd82fc3c55e53a32ead19e311d2de8abd0c4
Ali Nobariasl <freeeeboy.gh@gmail.com>
2023-08-07 15:14:56 +0300

    h

--- BookShop_main/orders/utils.py
+++ BookShop_main/orders/utils.py
@@ -3,5 +3,6 @@ import datetime
 
 def generate_order_numebr(pk):
     
-    current_datetime = datetime.datetime.now().strftime("%Y%m%d%H%S")  
+    current_datetime = datetime.datetime.now().strftime('%Y%m%d%H%M%S')  
     order_number = current_datetime + str(pk)
+    return order_number
